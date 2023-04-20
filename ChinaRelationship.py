2f2724c8bf6de6f55d7bb5e10245f7afd753ed76
aoguai <1340219674>
2023-04-20 18:20:41 +0800

    Refactoring code to fix bugs

--- ChinaRelationship.py
+++ ChinaRelationship.py
@@ -1,159 +1,77 @@
-# 【关系】f:父,m:母,h:夫,w:妻,s:子,d:女,xb:兄弟,ob:兄,lb:弟,xs:姐妹,os:姐,ls:妹
-# 【修饰符】 &o:年长,&l:年幼,#:隔断,[a|b]:并列
-
-# 在字符串指定位置插入字符
-# str_origin：源字符串  pos：插入位置  str_add：待插入的字符串
-#
-def strInsert(strOrigin, pos, strAdd):
-    strList = list(strOrigin)  # 字符串转list
-    strList.insert(pos, strAdd)  # 在指定位置插入字符串
-    strOut = ''.join(strList)  # 空字符连接
-    return strOut
+import json
+import re
 
 
-# 在字符串指定位置插入字符
-# str_origin：源字符串  pos：插入位置  str_add：待插入的字符串
-#
-def strInsert(strOrigin, pos, strAdd):
-    strList = list(strOrigin)  # 字符串转list
-    strList.insert(pos, strAdd)  # 在指定位置插入字符串
-    strOut = ''.join(strList)  # 空字符连接
-    return strOut
+class RelationshipCounter:
+    def __init__(self, data_file="data.json", filter_file="filter.json", reverse=False):
+        self.data = self.load_json(data_file)
+        self.filter = self.load_json(filter_file)["filter"]
+        self.reverse = reverse  # 是否反转
 
+    def load_json(self, file_name):
+        with open(file_name, "r", encoding="utf-8") as f:
+            return json.load(f)
 
-# 检查整个字符串是否包含中文
-def isChinese(string):
-    for ch in string:
-        if u'\u4e00' <= ch <= u'\u9fff':
-            return True
-
-    return False
-
-
-# 将文字转换成关系符号
-def transformTitleToKey(text):
-    result = text.replace("的", ",").replace("我", "").replace("爸爸", "f").replace("父亲", "f").replace("妈妈", "m").replace(
-        "母亲", "m").replace("爷爷", "f,f").replace("奶奶", "f,m").replace("外公", "m,f").replace("姥爷", "m,f").replace("外婆",
+    # 称谓转换成关联字符
+    def transform_title_to_key(self, string):
+        result = string.replace("的", ",").replace("我", "").replace("爸爸", "f").replace("父亲", "f").replace("妈妈", "m").replace(
+            "母亲", "m").replace("爷爷", "f,f").replace("奶奶", "f,m").replace("外公", "m,f").replace("姥爷", "m,f").replace("外婆",
                                                                                                                "m,m").replace(
-        "姥姥", "m,m").replace("老公", "h").replace("丈夫", "h").replace("老婆",
-                                                                   "w").replace(
-        "妻子", "h").replace("儿子", "s").replace("女儿", "d").replace("兄弟", "xd").replace("哥哥", "ob").replace("弟弟",
-                                                                                                         "lb").replace(
-        "姐妹", "xs").replace("姐姐", "os").replace("妹妹", "ls").strip(",")
-    return result
-
-
-def errorMessage(key):
-    message = key
-    if key.find("ob,h") != -1 or key.find("xb,h") != -1 or key.find("lb,h") != -1 or key.find("os,w") != -1 or key.find(
-            "ls,w") != -1 or key.find("xs,w") != -1 or key.find("f,h") != -1 or key.find("m,w") != -1 or key.find(
-            "d,w") != -1 or key.find("s,h") != -1:
-        message = "根据我国法律暂不支持同性婚姻，怎么称呼你自己决定吧"
-    elif (key.find("h,h") != -1 or key.find("w,w") != -1):
-        message = "根据我国法律暂不支持重婚，怎么称呼你自己决定吧"
-    return message
-
-
-# 去重和简化
-def FilteHelper(text):
-    result = text
-    filterName = './plugins/bot_ChinaRelationship/filter.json'  # filter.json文件路径
-    if not os.path.isfile(filterName):
-        return "filterName文件不存在"
-    with open(filterName, "r") as f:
-        obj = list(ijson.items(f, 'filter'))
-    for i in range(len(obj[0])):
-        users = obj[0][i]['exp']
-        if users == result:
-            return obj[0][i]['str']
-        elif re.match(obj[0][i]['exp'], result):  # 符合正则
-            result1 = re.findall(obj[0][i]['exp'], result)  # 返回string中所有与pattern匹配的全部字符串,返回形式为数组
-            a = 0
-            result2 = ""
-            if len(result1) > 1:
-                try:
-                    for i in len(result1):
-                        result = result.replace("$" + str(a + 1), result1[a])
-                        a = a + 1
-                    if result.find("#") != -1:
-                        result_l = result
-                        resultList = list(set(result_l.split("#")))  # # 是隔断符，所以分割文本
-                        for key in resultList:
-                            result = FilteHelper(key.strip(","))
-                            if (result.find("#") == -1):  # 当关系符号不含#时加入最终结果中
-                                result2 = result2 + result
-                        return result2
-                    else:
-                        return text
-                except Exception as e:
-                    return text
-            else:
-                return str(result1).replace("[\'", "").replace("\']", "")
-        elif re.match(obj[0][i]['exp'], strInsert(result, 0, ',')):  # 符合正则
-            result1 = re.findall(obj[0][i]['exp'], strInsert(result, 0, ','))  # 返回string中所有与pattern匹配的全部字符串,返回形式为数组
-            a = 0
-            result2 = ""
-            if len(result1) > 1:
-                try:
-                    for i in len(result1):
-                        result = result.replace("$" + str(a + 1), result1[a])
-                        a = a + 1
-                    if result.find("#") != -1:
-                        result_l = result
-                        resultList = list(set(result_l.split("#")))  # # 是隔断符，所以分割文本
-                        for key in resultList:
-                            result = FilteHelper(key.strip(","))
-                            if (result.find("#") == -1):  # 当关系符号不含#时加入最终结果中
-                                result2 = result2 + result
-                        return result2
-                    else:
-                        return text
-                except Exception as e:
-                    return text
-            else:
-                return str(result1).replace("[\'", "").replace("\']", "")
-    return text
-
-
-# 从数据源中查找对应 key 的结果
-def dataValueByKeys(data_text):
-    if (isChinese(data_text)):  # 判断是否含有中文，含有的是特殊回复
-        return data_text
-    dataName = './plugins/bot_ChinaRelationship/data.json'
-    if not os.path.isfile(dataName):
-        return "data文件不存在"
-    fo = open(dataName, 'r', encoding='utf-8')
-    ID_Data = demjson.decode(fo.read())
-    fo.close()
-    try:
-        if ID_Data[data_text]:
-            cityID = ID_Data[data_text]
-            text = ""
-            for key in cityID:
-                text = text + key + '\\'
-            return text.strip("\\")
-        else:
-            return "未找到"
-    except Exception as e:
-        result = ""
-        resultList = FilteHelper(strInsert(data_text, 0, ',')).split(",")规范代码
-        for key in resultList:
-            result = result + dataValueByKeys(key)
+            "姥姥", "m,m").replace("老公", "h").replace("丈夫", "h").replace("老婆", "w").replace("妻子", "h").replace("儿子",
+                                                                                                         "s").replace(
+            "女儿", "d").replace("兄弟", "xd").replace("哥哥", "ob").replace("弟弟", "lb").replace("姐妹", "xs").replace("姐姐",
+                                                                                                           "os").replace(
+            "妹妹", "ls").strip(",") + ","
+        for f in self.filter:
+            exp = "^" + f["exp"].replace("&", "\\&").replace(",", ".*") + "$"
+            result = re.sub(exp, f["str"].replace("$", "\\"), result)
+        if self.reverse:
+            result = result.replace("f", "m").replace("m", "f")
+        if result.endswith(","):
+            result = result[:-1]
         return result
 
+    # 错误关系判断
+    def error_message(self, key):
+        if key.find("ob,h") != -1 or key.find("xb,h") != -1 or key.find("lb,h") != -1 or key.find("os,w") != -1 or key.find(
+                "ls,w") != -1 or key.find("xs,w") != -1 or key.find("f,h") != -1 or key.find("m,w") != -1 or key.find(
+            "d,w") != -1 or key.find("s,h") != -1:
+            return "根据我国法律暂不支持同性婚姻，怎么称呼你自己决定吧"
+        elif key.find("h,h") != -1 or key.find("w,w") != -1:
+            return "根据我国法律暂不支持重婚，怎么称呼你自己决定吧"
+        return key
 
-def calculate(text):
-    keys = errorMessage(transformTitleToKey(text))
-    result = ""
-    resultList = list(set(dataValueByKeys(FilteHelper(keys)).split("\\")))
-    for key in resultList:
-        result = result + key.strip(",") + '\\'
-    contain_en = bool(re.search('[a-z]', result.strip("\\")))
-    if contain_en:
-        return "我不会"
-    else:
-        return result.strip("\\")
+    # 关系链转换成称谓
+    def transform_key_to_title(self, string):
+        if not string:
+            return None
+        result = []
+        seen = set()
+        for s in string.split("#"):
+            if s != self.error_message(s):
+                return self.error_message(s)
+            if s in self.data and self.data[s][0] not in seen:
+                result.append(self.data[s][0])
+                seen.add(self.data[s][0])
+        # 如果结果为空，再使用逗号分割子字符串
+        if not result:
+            for s in string.split(','):
+                if s != self.error_message(s):
+                    return self.error_message(s)
+                if s in self.data and self.data[s][0] not in seen:
+                    result.append(self.data[s][0])
+                    seen.add(self.data[s][0])
+        result_str = ','.join(result)
+        if self.reverse:
+            result_str = result_str.replace("父", "母").replace("母", "父")
+        return result_str
 
 
-txt = input()
-print(calculate(txt))
+if __name__ == '__main__':
+    rc = RelationshipCounter()
+    print(rc.transform_key_to_title(rc.transform_title_to_key("我的爸爸")))
+    print(rc.transform_key_to_title(rc.transform_title_to_key("我的父亲的儿子")))
+    print(rc.transform_key_to_title(rc.transform_title_to_key("我的哥哥的丈夫")))
+    print(rc.transform_key_to_title(rc.transform_title_to_key("我的哥哥的弟弟")))
+    print(rc.transform_key_to_title(rc.transform_title_to_key("我的爸爸的爸爸")))
+    print(rc.transform_key_to_title(rc.transform_title_to_key("我的哥哥的姐姐的妹妹")))
--- README.md
+++ README.md
@@ -1,157 +1,86 @@
 # ChinaRelationship
+
 一个简单的中国亲戚关系计算器 python实现版本
+
 每年的春节，都会有一些自己几乎没印象但父母就是很熟的亲戚，关系凌乱到你自己都说不清。
-今年趁着春节在家没事情干，正好之前知道有中国亲戚关系计算器，想着自己实现一下，特此记录。
-## 算法实现与介绍
-由于本人能力有限，只完成了基本功能....
-### 需求
-* 计算亲戚关系链得出我应该怎么称呼的结果
-### 数据定义
-    
+
+今年（2022年）趁着春节在家没事情干，正好之前知道有中国亲戚关系计算器，想着自己实现一下，特此记录。
+
+## 如何使用
+
+该程序的默认数据和过滤文件分别为`data.json`和`filter.json`。
+
+如果您需要更改这些文件的位置，请在类的构造函数中传入相应的参数。
+
+您可以按如下方式使用该程序：
+```python
+from RelationshipCounter import RelationshipCounter
+
+rc = RelationshipCounter()
+rc.transform_key_to_title(rc.transform_title_to_key("我的爸爸"))
+rc.transform_key_to_title(rc.transform_title_to_key("我的父亲的儿子"))
+rc.transform_key_to_title(rc.transform_title_to_key("我的哥哥的丈夫"))
+rc.transform_key_to_title(rc.transform_title_to_key("我的哥哥的弟弟"))
+rc.transform_key_to_title(rc.transform_title_to_key("我的爸爸的爸爸"))
+rc.transform_key_to_title(rc.transform_title_to_key("我的哥哥的姐姐的妹妹"))
+```
+
+## 算法与函数介绍
+
+简单介绍一下实现思路
+
+### 数据定义与准备
+
 1. 定义关系字符和修饰符
-```python
-【关系】f:父,m:母,h:夫,w:妻,s:子,d:女,xb:兄弟,ob:兄,lb:弟,xs:姐妹,os:姐,ls:妹
 
-【修饰符】 &o:年长,&l:年幼,#:隔断,[a|b]:并列
-```
+    关系：f:父,m:母,h:夫,w:妻,s:子,d:女,xb:兄弟,ob:兄,lb:弟,xs:姐妹,os:姐,ls:妹
+    
+    修饰符：&o:年长,&l:年幼,#:隔断,[a|b]:并列
+
 2. 关系对应数据集合、关系过滤数据集合（data.json 和 filter.json）
+3. 分析算法要兼容的结果
 
-原来参考的[作者](https://github.com/joywt/relationship)的关系过滤数据集合json有点问题，改了一下
+   需要解决的情况基本有以下三类：  
+    >我的爸爸 = 爸爸，  
+    
+    >我的哥哥的弟弟 = 自己/弟弟/哥哥，  
+    
+    >我的哥哥的老公 = ？  
 
- >**filter 数据集的用途：比如 m,h 是我的妈妈的丈夫就是爸爸，也就是 f。 filter 的作用是去重和简化，需要把 exp 用 str 进行替换**
- 
-### 算法实现
-需要解决的情况基本有以下三种：
->我的爸爸 = 爸爸，
+### 算法实现与函数说明
 
->我的哥哥的弟弟 = 自己/弟弟/哥哥，
+`transform_title_to_key`
 
->我的哥哥的老公 = ？
+将中文称呼转换为关联字符。例如，"我的爸爸"被转换为"f"。函数使用过滤器过滤掉不良关系。
 
-#### 分析
-三种结果：1.单结果 2.多结果 3.错误提示 ，那么我们的算法要兼容以上三种情况
-下面我们来一步步实现。
-
-#### 算法主要函数一：transformTitleToKey
-该函数主要负责**将文字转换成关系符号**
-```python
-# 将文字转换成关系符号
-def transformTitleToKey(text):
-    result = text.replace("的", ",").replace("我", "").replace("爸爸", "f").replace("父亲", "f").replace("妈妈","m").replace("母亲", "m").replace("爷爷","f,f").replace("奶奶", "f,m").replace("外公", "m,f").replace("姥爷", "m,f").replace("外婆", "m,m").replace("姥姥", "m,m").replace("老公","h").replace("丈夫", "h").replace("老婆", "w").replace("妻子", "h").replace("儿子", "s").replace("女儿", "d").replace("兄弟", "xd").replace("哥哥", "ob").replace("弟弟","lb").replace("姐妹","xs").replace("姐姐", "os").replace("妹妹", "ls").strip(",")
-    return result
-```
 这里简化了原参考[作者](https://github.com/joywt/relationship)的写法，更 ~~简单（不是）~~ 符合计算器设定
+
 ![计算器](https://github.com/aoguai/ChinaRelationship/blob/main/images/relationship_0.png)
-#### 算法主要函数二：FilteHelper
-该函数主要负责**去重和简化**
-```python
-# 去重和简化
-def FilteHelper(text):
-    result = text
-    filterName = 'D:/zm/ChinaRelationship-main/filter.json'  # filter.json文件路径
-    if not os.path.isfile(filterName):
-        return "filterName文件不存在"
-    with open(filterName, "r") as f:
-        obj = list(ijson.items(f, 'filter'))
-    for i in range(len(obj[0])):
-        users = obj[0][i]['exp']
-        if users == result:
-            return obj[0][i]['str']
-        elif re.match(obj[0][i]['exp'], result):  # 符合正则
-            result1 = re.findall(obj[0][i]['exp'], result)  # 返回string中所有与pattern匹配的全部字符串,返回形式为数组
-            print(result1)
-            a = 0
-            result2 = ""
-            if len(result1)>1:
-                try:
-                    for i in len(result1):
-                        result = result.replace("$" + str(a + 1), result1[a])
-                        a = a + 1
-                    if result.find("#") != -1:
-                        result_l = result
-                        resultList = list(set(result_l.split("#")))  # # 是隔断符，所以分割文本
-                        for key in resultList:
-                            result = FilteHelper(key.strip(","))
-                            if (result.find("#") == -1):  # 当关系符号不含#时加入最终结果中
-                                result2 = result2 + result
-                        return result2
-                    else:
-                        return text
-                except Exception as e:
-                    return text
-            else:
-                return str(result1).replace("[\'", "").replace("\']", "")
-        elif re.match(obj[0][i]['exp'], strInsert(result, 0, ',')):  # 符合正则
-            result1 = re.findall(obj[0][i]['exp'], strInsert(result, 0, ','))  # 返回string中所有与pattern匹配的全部字符串,返回形式为数组
-            a = 0
-            result2 = ""
-            if len(result1)>1:
-                try:
-                    for i in len(result1):
-                        result = result.replace("$" + str(a + 1), result1[a])
-                        a = a + 1
-                    if result.find("#") != -1:
-                        result_l = result
-                        resultList = list(set(result_l.split("#")))  # # 是隔断符，所以分割文本
-                        for key in resultList:
-                            result = FilteHelper(key.strip(","))
-                            if (result.find("#") == -1):  # 当关系符号不含#时加入最终结果中
-                                result2 = result2 + result
-                        return result2
-                    else:
-                        return text
-                except Exception as e:
-                    return text
-            else:
-                return str(result1).replace("[\'", "").replace("\']", "")
-    return text
-```
-这里原参考[作者](https://github.com/joywt/relationship)解释的有点乱，我就以我个人见解参考着写了出来...能跑....有错欢迎指出
-个人测试单结果，多结果都能实现，**建议多结果实现参考输出和代码详细理解**
-#### 算法主要函数三：dataValueByKeys
-该函数主要负责**从数据源中查找对应 key 的结果**
-```python
-# 从数据源中查找对应 key 的结果
-def dataValueByKeys(data_text):
-    if(isChinese(data_text)):  # 判断是否含有中文，含有的是特殊回复
-        return data_text
-    dataName = 'D:/zm/ChinaRelationship-main/data.json'  # data.json文件路径
-    if not os.path.isfile(dataName):
-        return "data文件不存在"
-    fo = open(dataName, 'r', encoding='utf-8')
-    ID_Data = demjson.decode(fo.read())
-    fo.close()
-    try:
-        if ID_Data[data_text]:
-            cityID = ID_Data[data_text]
-            text = ""
-            for key in cityID:
-                text = text + key + '\\'
-            return text.strip("\\")
-        else:
-            return "未找到"
-    except Exception as e:
-        result = ""
-        resultList = FilteHelper(strInsert(data_text, 0, ',')).split(",")
-        for key in resultList:
-            result = result + dataValueByKeys(key)
-        return result
-```
+
+`transform_key_to_title`
+
+将关联字符转换为中文称呼。例如，"f"被转换为"父亲"。
+
+`error_message`
+
+检查是否存在不合法关系链并返回相应错误信息。
 
 ### 输出与效果
+
 ![relationship_1](https://github.com/aoguai/ChinaRelationship/blob/main/images/relationship_1.png)
 ![relationship_2](https://github.com/aoguai/ChinaRelationship/blob/main/images/relationship_2.png)
 ![relationship_3](https://github.com/aoguai/ChinaRelationship/blob/main/images/relationship_3.png)
 ![relationship_4](https://github.com/aoguai/ChinaRelationship/blob/main/images/relationship_4.png)
- <br>基本达到效果
- 
+
+基本达到效果
+
 ## 一些细节与已知问题
- 首先，是性别：如果‘我’是女性，那么‘我的父亲的儿子’可以为[‘哥哥’，‘弟弟’]，而不可以包含‘我’。（上述代码没实现）<br>
-另外，关于夫妻关系：在正常情况下，男性称谓只可以有‘妻子’，女性称谓只可以有‘丈夫’。（上述代码已实现）<br>
-第三，多种可能：‘我的父亲的儿子’ 可以是[‘我’，‘哥哥’，‘弟弟’]，再若是再往后计算，如‘我的父亲的儿子的儿子’ ，需要同时考虑‘我的儿子’，‘哥哥的儿子’，‘弟弟的儿子’这三种可能。（上述代码已实现）<br>
+* 如果‘我’是女性，那么‘我的父亲的儿子’可以为[‘哥哥’，‘弟弟’]，而不可以包含‘我’。（上述代码未实现）
+* 关于夫妻关系：在正常情况下，男性称谓只可以有‘妻子’，女性称谓只可以有‘丈夫’。（上述代码已实现）
+* 多种可能：‘我的父亲的儿子’ 可以是[‘我’，‘哥哥’，‘弟弟’]，再若是再往后计算，如‘我的父亲的儿子的儿子’ ，需要同时考虑‘我的儿子’，‘哥哥的儿子’，‘弟弟的儿子’这三种可能。（上述代码已实现）
+
 已知问题：某些涉及自己的多重可能还存在莫名BUG
 
 ## 参考
-站在巨人的肩膀上
-### [hinese kinship system.中国亲戚关系计算器 - 家庭称谓/称呼计算/亲戚关系算法](https://github.com/mumuy/relationship)
-### 算法主要参考了这个[亲戚关系计算器](https://github.com/joywt/relationship)
+[hinese kinship system.中国亲戚关系计算器 - 家庭称谓/称呼计算/亲戚关系算法](https://github.com/mumuy/relationship)
+算法主要参考了[亲戚关系计算器](https://github.com/joywt/relationship)
